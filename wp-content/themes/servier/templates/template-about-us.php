06f8a29ebbcc56d68a64c0baf67fcefe1d62c1d4
Andrei Dumitru <thesemeth@gmail.com>
2021-09-29 19:08:15 +0300

    about page and children pages

--- wp-content/themes/servier/templates/template-about-us.php
+++ wp-content/themes/servier/templates/template-about-us.php
@@ -9,4 +9,4 @@ $context['thumbnail'] = get_the_post_thumbnail_url();
 $context['base_url'] = get_template_directory_uri();
 $context['posts'] = Timber::get_posts($posts);
 
-Timber::render('views/pages/new_page.twig', $context);
+Timber::render('views/pages/page-about-us.twig', $context);
