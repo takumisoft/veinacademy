9c7322cad3119deb2f8a3054df5296e0608687e1
Andrei Dumitru <thesemeth@gmail.com>
2021-09-29 21:37:22 +0300

    homepage

--- wp-content/themes/servier/front-page.php
+++ wp-content/themes/servier/front-page.php
@@ -9,10 +9,17 @@ $posts = [
     'order' => 'ASC',
     'posts_per_page' => 10,
 ];
+$new_posts = [
+    'post_type' => ['news', 'website', 'apps', 'library'],
+    'orderby' => 'date',
+    'order' => 'DESC',
+    'posts_per_page' => 4
+];
 
 $context['frontpage'] = true;
 $context['thumbnail'] = get_the_post_thumbnail_url();
 $context['base_url'] = get_template_directory_uri();
+$context['new_posts'] = Timber::get_posts($new_posts);
 $context['posts'] = Timber::get_posts($posts);
 
 Timber::render('views/pages/new_home.twig', $context);
