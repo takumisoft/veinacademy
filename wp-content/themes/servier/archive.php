547e42add0d244f8487796e03dfcb9ebfbcef2e9
Andrei Dumitru <thesemeth@gmail.com>
2021-09-29 14:33:21 +0300

    news file change and updated

--- wp-content/themes/servier/archive.php
+++ wp-content/themes/servier/archive.php
@@ -1,105 +1,73 @@
 <?php
-/**
- * The template for displaying Archive pages.
- */
-
-
-$templates = ['views/pages/new_page.twig'];
-
+/* Template Name: New News */
 $context = Timber::get_context();
-$context['title'] = get_the_archive_title();
+$post = new TimberPost();
+$context['post'] = $post;
 
-$queried_object = get_queried_object();
-$archive_description = get_the_archive_description();
-
-$archive_banner_image = false;
-$archive_presentation = false;
-
-if (is_post_type_archive()) {
-
-    $context['title'] = post_type_archive_title('', false);
-    // Image post types
-    $archive_banner_image_option = get_field('pt_'.get_post_type().'_archive_banner_image', 'option');
-    $has_archive_banner_image_option = !is_null($archive_banner_image_option) && !empty($archive_banner_image_option);
-    $archive_banner_image = $has_archive_banner_image_option ? $archive_banner_image_option['url'] : get_stylesheet_directory_uri() . '/assets/img/banner/banner-' . get_post_type() . '.jpg';
-    if (is_post_type_archive('news')) {
-        $context['categories'] = Timber::get_terms('category', [
-                'post_type'  => 'news',
-                'hide_empty' => true,
-                'orderby'    => 'name',
-                'exclude'    => 1,
-                'order'      => 'ASC']
-        );
+$link = $_SERVER["REQUEST_URI"];
+$uris = explode('/', $link);
+for($i = 0; $i < count($uris); $i++){
+    if($uris[$i] == 'page'){
+        $paged = $uris[$i + 1];
     }
-
-    //	array_unshift( $templates, 'views/pages/' . get_post_type() . '/archive-' . get_post_type() . '.twig' );
-
-
-} elseif (is_tax()) {
-    $context['title'] = single_cat_title('', false);
-    $archive_presentation = get_field('archive_presentation', $queried_object);
-    $archive_banner_image = get_field('archive_banner_image', $queried_object) ? get_field('archive_banner_image', $queried_object)['url'] : get_stylesheet_directory_uri() . '/assets/img/banner/banner-' . get_post_type() . '.jpg';
-	$context['libpublications'] = Timber::get_terms('library-type', [
-		'orderby' => 'name',
-		'order'   => 'ASC',
-		'hide_empty' => true,
-		'parent'  => 52]
-	);
-	$context['libinterviews'] = Timber::get_terms('library-type', [
-		'orderby' => 'name',
-		'order'   => 'ASC',
-		'hide_empty' => true,
-		'parent'  => 11]
-	);
+}
+if(!isset($paged) || !$paged){
+    $paged = 1;
+}
+if(!isset($_GET['filter']) || !$_GET['filter']){
+	$filter = [];
+    $terms = get_terms(['taxonomy' => 'category','hide_empty' => false,]);
+	foreach($terms as $t){
+		if($t->count){
+			array_push($filter, $t->slug);
+		}
+	}
+}else{
+    $filter = $_GET['filter'];
 }
 
-$context['library_types'] = Timber::get_terms('library-type', [
-        'hide_empty' => true,
-        'orderby'    => 'name',
-        'order'      => 'DESC']
-);
-
-
-if (isset($_GET['libcat']) && !empty( $_GET['libcat'] )) {
-	$order = isset($_GET['order'])?'ASC':'DESC';
-	$posts_libcat = [
-		'post_type' => 'library',
-		'orderby' => 'date',
-		'order' => $order,
-		'tax_query' => [
-			[
-				'taxonomy' => 'library-type',
-				'field'    => 'term_id',
-				'terms'    => $_GET['libcat'],
-			],
-		],
-	];
-	$posts = Timber::get_posts($posts_libcat);
-
-} else {
-	$posts = Timber::get_posts();
-	$context['pagination'] = Timber::get_pagination([
-		'end_size' => 1,
-		'mid_size' => 2,
-	]);
+if(!isset($_GET['order']) || !$_GET['order']){
+	$order = 'DESC';
+}else{
+	$order = $_GET['order'];
 }
-
-
-// Sort a post news orderby Re-order plugin
-if (is_post_type_archive('news')) {
-    usort($posts, "cmp");
+if(!isset($_GET['orderby']) || !$_GET['orderby']){
+	$orderby = 'date';
+}else{
+	$orderby = $_GET['orderby'];
 }
-$context['posts'] = $posts;
-$context['archive_description'] = strip_tags($archive_description);
-$context['archive_presentation'] = $archive_presentation;
-$context['archive_banner_image'] = $archive_banner_image;
+$posts = [
+    'post_type' => 'news',
+    'posts_per_page' => 10,
+    'orderby' => $orderby,
+    'order' => $order,
+    'paged' => $paged,
+	'tax_query' => [
+        [
+            'taxonomy' => 'category',
+            'field'    => 'slug',
+            'terms'    => $filter,
+        ],
+    ],
+];
+$context['pagination'] = Timber::get_pagination([
+	'end_size' => 1,
+	'mid_size' => 2,
+]);
 
+$context['posts_type'] = 'vein-news';
+$context['posts'] = new Timber\PostQuery($posts);
+$filters = get_terms('category', array('parent' => '0'));
+$context['filters'] = $filters;
+$context['filt'] = $filter;
+$context['frontpage'] = false;
 
+$context['post']->post_title = 'News';
 $context['base_url'] = get_template_directory_uri();
+$context['thumbnail'] = site_url() . '/wp-content/uploads/banner-news.jpg';
 
-Timber::render($templates, $context);
+$no_page_url = get_pagenum_link();
+$clean_url = explode('?', $no_page_url)[0];
+$context['clean_url'] = $clean_url;
 
-function cmp($a, $b)
-{
-    return $a->menu_order - $b->menu_order;
-}
+Timber::render('views/pages/new_page.twig', $context);
