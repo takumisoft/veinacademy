876d3fa1cb283869f4415121daf2d55ae2aff0c1
takumisoft <chl0mIuN>
2021-09-30 03:00:48 +0300

    functions

--- wp-content/themes/servier/functions.php
+++ wp-content/themes/servier/functions.php
@@ -28,14 +28,23 @@ function class_loader() {
 }
 
 function theme_load_scripts() {
-	wp_enqueue_script('jquery-ui-accordion');
+    wp_enqueue_script('jquery-ui-accordion');
     wp_enqueue_style( 'style-default', get_stylesheet_uri() );
     wp_enqueue_script( 'select2-script', 'https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js' );
-    wp_enqueue_script( 'new-script', get_template_directory_uri() . '/new-assets/js/new_script.js?v=1' );
+    wp_enqueue_script( 'new-script', get_template_directory_uri() . '/new-assets/js/new_script.js' );
     wp_enqueue_style( 'select2-css', 'https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css' );
     wp_enqueue_style( 'new-style', get_template_directory_uri() . '/new-assets/style.css' );
 }
 add_action( 'wp_enqueue_scripts', 'theme_load_scripts' );
+
+function reading_time($id){
+    print_r(get_post($id));
+}
+
+
+function get_word_count($content){
+    return str_word_count($content) / 250 . ' min read';
+}
 
 function wpdocs_theme_name_scripts() {
     wp_enqueue_style( 'csccs', get_template_directory_uri() . '/assets/css/custom.css' );
@@ -82,3 +91,31 @@ function get_post_categories( $post_id ) {
     return $terms;
 
 };
+
+/****
+** Defer styles and scripts
+***/
+
+// CSS
+function add_rel_preload($html, $handle, $href, $media) {
+    
+    if (is_admin())
+        return $html;
+
+     $html = <<<EOT
+<link rel='preload' as='style' onload="this.onload=null;this.rel='stylesheet'" id='$handle' href='$href' type='text/css' media='all' />
+EOT;
+    return $html;
+}
+add_filter( 'style_loader_tag', 'add_rel_preload', 10, 4 );
+
+// JS
+add_filter( 'clean_url', function( $url )
+{
+    if ( FALSE === strpos( $url, '.js' ) )
+    { // not our file
+        return $url;
+    }
+    // Must be a ', not "!
+    return "$url' defer='defer";
+}, 11, 1 );
